7ea5dcb55f738b0b9661ec38b569c1a987a1eb0e
sunkplane <andrezito300@gmail.com>
2022-05-01 11:52:03 -0300

    remove some leftovers

--- graph.sh
+++ graph.sh
@@ -3,5 +3,7 @@
 gfortran graph.f90 -o graph.out
 ./graph.out
 
-g++ graph.cpp $(root-config --glibs --cflags --libs) -o graph_app
-./graph_app graph1.dat graph2.dat
+mkdir -p bin
+
+g++ graph.cpp $(root-config --glibs --cflags --libs) -o bin/graph_app
+bin/graph_app graph1.dat graph2.dat
