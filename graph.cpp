c853929254618951a8d4bb4338be8d59f5dafcef
sunkplane <andrezito300@gmail.com>
2022-07-21 14:07:45 -0300

    make marker flag and change line style

--- graph.cpp
+++ graph.cpp
@@ -28,13 +28,48 @@ std::vector<std::string> getGraphFiles(int argc, char **argv) {
 	files.reserve(argc - 1);
 
 	for (int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			continue;
+		}
 		files.push_back(std::string(argv[i]));
 	}
 
 	return files;
 }
 
-std::unique_ptr<TMultiGraph> createGraph(std::vector<std::string> files) {
+std::string getFlag(int argc, char **argv, const char *flag) {
+	std::string flag_val = "";
+
+	for (int i = 1; i < argc; i++) {
+	    std::string arg_string = argv[i];
+		size_t arg_pos = arg_string.find(flag);
+		if (arg_pos != std::string::npos) {
+			size_t flag_val_pos = arg_string.find("=")+1;
+			flag_val = arg_string.substr(flag_val_pos);
+		}
+    }
+
+    return flag_val;
+}
+
+bool get_marker_set(int argc, char **argv) {
+	std::string marker_set_flag = getFlag(argc, argv, "-marker");
+	std::cout << marker_set_flag;
+
+	if (marker_set_flag == "off") {
+		return false;
+	}
+
+	return true;
+}
+
+struct GraphOptions {
+	bool ShowMarker;
+}
+
+const DefaultGraphOptions = GraphOptions{.ShowMarker = true};
+
+std::unique_ptr<TMultiGraph> createGraph(std::vector<std::string> files, GraphOptions graph_opt = DefaultGraphOptions) {
 	auto mg = std::make_unique<TMultiGraph>();
 
 	int i = 2;
@@ -45,9 +80,18 @@ std::unique_ptr<TMultiGraph> createGraph(std::vector<std::string> files) {
 		// resource, since when TMultiGraph is delete, so will it's underlying graphs
 		TGraph *graph = new TGraph(file.c_str(), "%lg %lg");
 		graph->SetTitle(("Function graph define by data file " + file).c_str());
+
 		graph->SetLineWidth(3);
+		graph->SetLineStyle(i);
+
 		graph->SetMarkerStyle(i);
 		graph->SetMarkerSize(1);
+
+		if (!graph_opt.ShowMarker) {
+			graph->SetMarkerStyle(0);
+			graph->SetMarkerSize(0);
+		}
+
 		graph->SetEditable(kFALSE);
 		mg->Add(graph, "PL");
 
@@ -66,7 +110,12 @@ int main(int argc, char **argv) {
 	gStyle->SetOptTitle(kFALSE);
 	gStyle->SetPalette(kSolar);
 
-	std::unique_ptr<TMultiGraph> graph = createGraph(files);
+	GraphOptions graph_opt = DefaultGraphOptions;
+	if (!get_marker_set(argc, argv)) {
+		graph_opt.ShowMarker = false;
+	}
+
+	std::unique_ptr<TMultiGraph> graph = createGraph(files, graph_opt);
 	graph->SetTitle("Graph; x; f(x)");
 	graph->Draw("as pmc plc");
 
